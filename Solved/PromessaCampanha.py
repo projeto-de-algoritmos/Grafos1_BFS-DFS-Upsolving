c6cd5f1400b854506f9053d1705ed64cb61d0b73
RafaelBP02 <rafaelbertopereira@gmail.com>
2022-06-24 12:47:20 -0300

    Inicio da resolucao da questao BEE 1835(Beecrowd antigo URI) . > > Co-authored-by: Guilherme Braz <gvcerveirabraz@gmail.com>

---
+++ Solved/PromessaCampanha.py
@@ -0,0 +1,16 @@
+def Graph(numNodes, edges):
+    
+    nodes = [a for a in range(1,numNodes)]
+    
+    
+    
+
+
+t = int(input)
+
+
+
+for i in range(t):
+    numNodes, numEdges = map(int, input.split()) 
+    
+    edges = list(map(int, input.split()))
